2da46335fac05e851a61f1a2554c6132be43ab11
Sachin Jain <sachin.lnm@gmail.com>
2020-09-06 13:18:24 +0530

    Removing all the changes

--- GitTesting/src/main/java/demo/Application.java
+++ GitTesting/src/main/java/demo/Application.java
@@ -6,8 +6,6 @@ package demo;
  * 
  * Change A
  * Change B
- * Change C
- * Change XY
  * 
  * @author Dell
  *
@@ -15,6 +13,5 @@ package demo;
 public class Application {
 	public static void main(String[] args) {
 		System.out.println("Jai Hanumaan Jee Kee!!");
-		//Change D
 	}
 }
